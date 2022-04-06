72f54f00b879df031d1d51263693940cddeb9b24
Kittichon <>
2022-04-06 21:17:46 +0700

    Plus default empty data list

--- lib/src/utils/ai_global_const.dart
+++ lib/src/utils/ai_global_const.dart
@@ -1,2 +1,8 @@
 const kFullCurrencyFormat = '#,###.00';
 const kDecimalFormat = '#,###';
+
+const kEmptyStringList = <String>[];
+const kEmptyIntList = <int>[];
+const kEmptyDoubleList = <double>[];
+const kEmptyDateTimeList = <DateTime>[];
+const kEmptyDynamicList = <dynamic>[];
