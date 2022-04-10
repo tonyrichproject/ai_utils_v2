d1d58ad86a4e943f4018223e556f2c4b3ba65c10
Kittichon <>
2022-04-10 14:13:25 +0700

    Adding safe value as string utility function

--- lib/src/utils/ai_basic_utils.dart
+++ lib/src/utils/ai_basic_utils.dart
@@ -1,4 +1,39 @@
+import 'package:ai_base_class_v2/ai_base_class_v2.dart';
+
 import '../models/ai_base_model.dart';
 
 bool assigned(dynamic aObject) => (aObject != null) ? true : false;
 bool isAvailable(AiBaseItem aObject) => (assigned(aObject) && aObject.isNotEmpty);
+
+String safeValueAsStr(dynamic aValue, {String aDefaultValue = ''}) {
+  String result = aDefaultValue;
+  if (assigned(aValue)) {
+    if (aValue is String)
+      result = aValue;
+    else {
+      try {
+        aValue.toString();
+      } catch (error) {
+        print('Safe value as string error with message : ${error.toString()}');
+        // throw error;
+      }
+    }
+  }
+  return result;
+}
+
+int safeValueAsInt(dynamic aValue, {int aDefaultValue = 0}) {
+  int result = aDefaultValue;
+  return result;
+}
+
+double safeValueAsDouble(dynamic aValue, {double aDefaultValue = 0.0}) {
+  double result = aDefaultValue;
+  return result;
+}
+
+DateTime safeValueAsDateTime(dynamic aValue, {DateTime? aDefaultValue}) {
+  DateTime result = kDefaultDateTime;
+  if (assigned(aDefaultValue)) result = aDefaultValue!;
+  return result;
+}
