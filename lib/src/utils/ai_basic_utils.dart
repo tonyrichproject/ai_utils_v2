812211c606ee5808a7547d5954c7fd39b292ed2a
Kittichon <>
2022-04-13 03:04:28 +0700

    Update functions

--- lib/src/utils/ai_basic_utils.dart
+++ lib/src/utils/ai_basic_utils.dart
@@ -7,51 +7,101 @@ bool isAvailable(AiBaseItem aObject) => (assigned(aObject) && aObject.isNotEmpty
 
 String safeValueAsStr(dynamic aValue, {String aDefaultValue = ''}) {
   String result = aDefaultValue;
-  if (assigned(aValue)) {
+  try {
     if (aValue is String)
       result = aValue;
     else {
-      try {
-        if (aValue is int)
-          result = aValue.toString();
-        else if (aValue is double)
-          result = aValue.toString();
-        else if (aValue is DateTime) result = aValue.toString();
-      } catch (error) {
-        print('Safe value as string error with message : ${error.toString()}');
-        // throw error;
-      }
+      if (aValue is int)
+        result = aValue.toString();
+      else if (aValue is double)
+        result = aValue.toString();
+      else if (aValue is num)
+        result = aValue.toString();
+      else if (aValue is DateTime) result = aValue.toString();
     }
+  } catch (error) {
+    return aDefaultValue;
   }
+  // if (assigned(aValue)) {
+  //   if (aValue is String)
+  //     result = aValue;
+  //   else {
+  //     try {
+  //       if (aValue is int)
+  //         result = aValue.toString();
+  //       else if (aValue is double)
+  //         result = aValue.toString();
+  //       else if (aValue is DateTime) result = aValue.toString();
+  //     } catch (error) {
+  //       print('Safe value as string error with message : ${error.toString()}');
+  //       // throw error;
+  //     }
+  //   }
+  // }
   return result;
 }
 
 int safeValueAsInt(dynamic aValue, {int aDefaultValue = 0}) {
   int result = aDefaultValue;
-  if (assigned(aValue)) {
+  try {
     if (aValue is int)
       result = aValue;
     else {
-      try {
-        // if (aValue is String)
-        //   result = int.parse(aValue.trim());
-        // else
-        if (aValue is double) result = aValue.floor();
-      } catch (error) {
-        print('Safe value as int error with message : ${error.toString()}');
-      }
+      if (aValue is double)
+        result = aValue.floor();
+      else if (aValue is String)
+        result = int.parse(aValue.trim());
+      else if (aValue is num) result = aValue.toInt();
     }
+  } catch (error) {
+    return aDefaultValue;
   }
+  // if (assigned(aValue)) {
+  //   if (aValue is int)
+  //     result = aValue;
+  //   else {
+  //     try {
+  //       // if (aValue is String)
+  //       //   result = int.parse(aValue.trim());
+  //       // else
+  //       if (aValue is double) result = aValue.floor();
+  //     } catch (error) {
+  //       print('Safe value as int error with message : ${error.toString()}');
+  //     }
+  //   }
+  // }
   return result;
 }
 
 double safeValueAsDouble(dynamic aValue, {double aDefaultValue = 0.0}) {
   double result = aDefaultValue;
+  try {
+    if (aValue is double)
+      result = aValue;
+    else {
+      if (aValue is int)
+        result = aValue * 1.0;
+      else if (aValue is String)
+        result = double.parse(aValue.trim());
+      else if (aValue is num) result = aValue.toDouble();
+    }
+  } catch (error) {
+    return aDefaultValue;
+  }
   return result;
 }
 
 DateTime safeValueAsDateTime(dynamic aValue, {DateTime? aDefaultValue}) {
   DateTime result = kDefaultDateTime;
-  if (assigned(aDefaultValue)) result = aDefaultValue!;
+  try {
+    if (aValue is DateTime)
+      result = aValue;
+    else {
+      // if (aValue is String) result = DateTime
+    }
+  } catch (error) {
+    return aDefaultValue!;
+  }
+  // if (assigned(aDefaultValue)) result = aDefaultValue!;
   return result;
 }
